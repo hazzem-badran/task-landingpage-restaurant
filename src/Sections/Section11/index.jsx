9bb71f7e045fbceacc6f9baf6c20738ba72327ff
Hazzembadran <hazemaqbadran@gmail.com>
2024-11-14 12:01:00 +0200

    make mail section more beter

--- src/Sections/Section11/index.jsx
+++ src/Sections/Section11/index.jsx
@@ -4,9 +4,9 @@ import EmailPage from '../../Components/Email/EmailSection'
 export default class Section11 extends Component {
   render() {
     return (
-      <div>
+      <>
         <EmailPage />
-      </div>
+      </>
     )
   }
 }
