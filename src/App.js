d5f10783b7b931407109609f5a86426190eec849
Hazzembadran <hazemaqbadran@gmail.com>
2025-04-21 20:37:50 +0300

    refactor App component to remove unused imports and implement lazy loading for HomePage

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import Section1 from './Sections/Section1';
-import Section2 from './Sections/Section2';
-import Section3 from './Sections/Section3';
-import Section4 from './Sections/Section4';
-import Section5 from './Sections/Section5';
-import Section6 from './Sections/Section6';
-import Section7 from './Sections/Section7';
-import Section8 from './Sections/Section8';
-import Section9 from './Sections/Section9';
-import Section10 from './Sections/Section10';
-import Section11 from './Sections/Section11';
-import Section12 from './Sections/Section12';
-import ReviewsPage from './Components/Reviews/ReviewsSection';
-import HomePage from './Pages/HomePage';
+
 import Loader from './Components/Loader';
 import { Suspense } from 'react';
+import { lazy } from 'react';
 
+const HomePage = lazy(() => import('./Pages/HomePage'));
 
 
 function App() {
   return (
     <div className="App">
     <Suspense fallback={<Loader />}>
-    <Loader />
-      {/* <HomePage /> */}
+      <HomePage />
     </Suspense>
     </div>
   );
